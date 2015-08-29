01e22fdf00acd2516cc5e01b43982b445900edd6
Brad Murphy <brad.murphy@byhook.com>
2015-08-29 18:17:32 -0400

    Initial set up of landing page.  All styles are in, still need to scripting and animation.

---
+++ app/config.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var config = {
+  manifest: [
+    {src: './images/logo.png'},
+    {src: './images/hero.jpg'},
+    {src: './images/fb.png'},
+    {src: './images/ig.png'},
+    {src: './images/bg.jpg'}
+  ]
+};
+
+module.exports = config;
+
---
+++ app/load.js
@@ -0,0 +1,6 @@
+'use strict';
+
+var config = require('./config');
+var Loader = require('./core/Loader');
+
+var loader = new Loader(config.manifest);
