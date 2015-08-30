93a36207a409a036a3079140529552c8c2fb1a69
Brad Murphy <brad.murphy@byhook.com>
2015-08-30 16:47:07 -0400

    Reworked animation settings.

--- app/animation.js
+++ app/animation.js
@@ -5,11 +5,17 @@ var browser = require('bowser');
 
 var Animation = function() {
 
-	if (!browser.mobile && !browser.tablet) {
+	if (browser.mobile || browser.tablet) {
+
+		TweenMax.set('.animate',{ autoAlpha: 1 });
+
+	} else {
 
 		TweenMax.staggerTo('.animate', 0.35, { autoAlpha: 1, ease: Power4.easeIn }, 0.2);
 
 	}
+
+
 
 };
 
