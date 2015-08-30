8c2537122140ce9db6e9cae918168a36efcbb8fa
Brad Murphy <brad.murphy@byhook.com>
2015-08-30 16:02:36 -0400

    Remove map object on mobile devices.  Set map to function on tablet.  Fixed nav width.

--- app/app.js
+++ app/app.js
@@ -13,52 +13,53 @@ app.controller('jcLocationCtrl', ['$scope', function($scope) {
 
 	$scope.locations = config.locations;
 
-	if (!browser.mobile) {
+	var latLng = new google.maps.LatLng(33.975, -84.387982);
 
-		var latLng = new google.maps.LatLng(33.975, -84.387982);
+	var mapOptions = {
+    zoom: 8,
+    center: latLng,
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    scrollwheel: false
+  };
 
-		var mapOptions = {
-	    zoom: 8,
-	    center: latLng,
-	    mapTypeId: google.maps.MapTypeId.ROADMAP,
-	    scrollwheel: false
-	  };
+	$scope.map = new google.maps.Map(document.getElementById('map'), mapOptions);
 
-		$scope.map = new google.maps.Map(document.getElementById('map'), mapOptions);
+	$scope.markers = [];
+	
+	var infoWindow = new google.maps.InfoWindow();
+	
+	var createMarker = function (info){
+	    
+	    var marker = new google.maps.Marker({
+	        map: $scope.map,
+	        position: new google.maps.LatLng(info.lat, info.lng),
+	        title: info.name
+	    });
 
-		$scope.markers = [];
-		
-		var infoWindow = new google.maps.InfoWindow();
-		
-		var createMarker = function (info){
-		    
-		    var marker = new google.maps.Marker({
-		        map: $scope.map,
-		        position: new google.maps.LatLng(info.lat, info.lng),
-		        title: info.name
-		    });
+	    marker.content = '<div class="info-block window">'
+	    + '<span>' + info.address + '</span>'
+	    + '<span>' + info.cityState + '</span>'
+	   	+ '<a href="' + 'tel:' + info.telLink + '">' + info.tel + '</a>' + '<br/>'
+	   	+ '<a href="' + info.dir + '" target="_blank">Directions</a> | '
+	   	+ '<a href="' + info.web + '" target="_blank">Website</a>'
+	    + '</div>';
 
-		    marker.content = '<div class="info-block window">'
-		    + '<span>' + info.address + '</span>'
-		    + '<span>' + info.cityState + '</span>'
-		   	+ '<a href="' + 'tel:' + info.telLink + '">' + info.tel + '</a>' + '<br/>'
-		   	+ '<a href="' + info.dir + '" target="_blank">Directions</a> | '
-		   	+ '<a href="' + info.web + '" target="_blank">Website</a>'
-		    + '</div>';
+	    google.maps.event.addListener(marker, 'click', function(){
+	        infoWindow.setContent('<h2>' + marker.title + '</h2>' + marker.content);
+	        infoWindow.open($scope.map, marker);
+	    });
 
-		    google.maps.event.addListener(marker, 'click', function(){
-		        infoWindow.setContent('<h2>' + marker.title + '</h2>' + marker.content);
-		        infoWindow.open($scope.map, marker);
-		    });
+	    $scope.markers.push(marker);
 
-		    $scope.markers.push(marker);
+	};
+	
+	for (var i = 0; i < config.locations.length; i++){
+	  createMarker(config.locations[i]);
+	}
 
-		};
-		
-		for (var i = 0; i < config.locations.length; i++){
-		    createMarker(config.locations[i]);
-		}
-
+	if (browser.mobile) {
+		this.map = document.querySelector('#map');
+		this.map.parentNode.removeChild(this.map);
 	}
 
 }]);
