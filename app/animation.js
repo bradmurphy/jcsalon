36a918243a6c8372300d12a33bf4bcc5bc8417ca
Brad Murphy <brad.murphy@byhook.com>
2015-08-30 16:36:46 -0400

    Fixed loading animations for desktop.

--- app/animation.js
+++ app/animation.js
@@ -1,20 +1,15 @@
 'use strict';
 
 var TweenMax = require('TweenMax');
+var browser = require('bowser');
 
 var Animation = function() {
 
-	this.hero = document.querySelector('.hero');
-	this.locationHeader = document.querySelector('.location h1');
-	this.locationCopy = document.querySelector('.location p');
-	this.map = document.querySelector('#map');
+	if (!browser.mobile && !browser.tablet) {
 
-	TweenMax.staggerTo([
-			this.hero,
-			this.locationHeader,
-			this.locationCopy,
-			this.map
-		], 0.5, { autoAlpha: 1, ease: Power4.easeIn }, '0.2');
+		TweenMax.staggerTo('.animate', 0.35, { autoAlpha: 1, ease: Power4.easeIn }, 0.2);
+
+	}
 
 };
 
